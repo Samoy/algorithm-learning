ebf15e30cae48bd49467b02488bff346b28b7518
samoy <samoy_young@163.com>
2024-05-06 09:14:39 +0800

    016. 链表的中间结点

--- 016.链表的中间结点/main.cpp
+++ 016.链表的中间结点/main.cpp
@@ -6,11 +6,26 @@ using namespace std;
 class Solution {
 public:
     ListNode *middleNode(ListNode *head) {
-
+        ListNode *current = head;
+        int i = 0;
+        while (current != nullptr) {
+            current = current->next;
+            i++;
+        }
+        ListNode *middle = head;
+        for (int j = 1; j <= i / 2; ++j) {
+            middle = middle->next;
+        }
+        return middle;
     }
 };
 
 int main(int argc, char const *argv[]) {
-
+    Solution s{};
+    ListNode *node1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    ListNode *node2 = new ListNode(1,
+                                   new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+    cout << s.middleNode(node1) << endl;
+    cout << s.middleNode(node2) << endl;
     return 0;
 }
