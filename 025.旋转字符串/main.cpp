4683bab1d8eceb074fd78dd69e771bc9c0f5a147
samoy <samoy_young@163.com>
2024-05-07 09:30:17 +0800

    025. 旋转字符串

--- 025.旋转字符串/main.cpp
+++ 025.旋转字符串/main.cpp
@@ -6,16 +6,20 @@ using namespace std;
 
 class Solution {
 public:
+    /*
+     * 基本思想：
+     * 1.长度检查：首先，通过比较 s 和 goal 的长度来确保它们能够通过旋转得到彼此，
+     * 这是必要条件。如果长度不相等，直接返回 false。
+     *
+     * 2.旋转验证：利用字符串拼接 (goal + goal)，这个操作实质上是在不真正进行多次旋转的情况下，
+     * 检查 s 是否能作为 goal 的某个旋转形式存在。因为如果 s 可以通过旋转得到 goal，
+     * 那么它必然作为子串出现在连续两次的 goal 中。
+     *
+     * 3. 查找操作：使用 std::string::find 方法查找 s 是否为 (goal + goal) 的子串。
+     * 如果 find 返回的不是 -1，说明找到了匹配，即 s 可以通过旋转变为 goal，此时返回 true；否则返回 false。
+     */
     bool rotateString(string s, string goal) {
-        for (int i = 0; i < s.size(); ++i) {
-            char tmp = s[0];
-            s += tmp;
-            s.erase(s.begin());
-            if (s == goal) {
-                return true;
-            }
-        }
-        return false;
+        return s.length() == goal.length() && (goal + goal).find(s) != -1;
     }
 };
 
