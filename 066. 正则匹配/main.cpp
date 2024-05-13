087fe9e690c5dabd5e8eeb978e9ea1d393945195
samoy <samoy_young@163.com>
2024-05-13 16:19:14 +0800

    066. 正则匹配

---
+++ 066. 正则匹配/main.cpp
@@ -0,0 +1,51 @@
+#include "../include/node.h"
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+
+        auto matches = [&](int i, int j) {
+            if (i == 0) {
+                return false;
+            }
+            if (p[j - 1] == '.') {
+                return true;
+            }
+            return s[i - 1] == p[j - 1];
+        };
+
+        vector<vector<int>> f(m + 1, vector<int>(n + 1));
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    f[i][j] |= f[i][j - 2];
+                    if (matches(i, j - 1)) {
+                        f[i][j] |= f[i - 1][j];
+                    }
+                }
+                else {
+                    if (matches(i, j)) {
+                        f[i][j] |= f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+};
+
+
+int main(int argc, char const *argv[]) {
+    Solution s{};
+    cout << s.isMatch("aa", "a") << endl;
+    cout << s.isMatch("aa", "a*") << endl;
+    cout << s.isMatch("ab", ".*") << endl;
+    cout << s.isMatch("abc", "a***abc") << endl;
+    return 0;
+}
